806261a75a3f3cb493eee171d65306b7be3f5e3f
arrivo <daffarrivo@gmail.com>
2026-04-09 08:05:41 +0700

    page1 first commit

---
+++ app/src/main/java/com/example/lokacaruy/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.lokacaruy
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnAboutProject = findViewById<Button>(R.id.btnGoToProfile)
+        val btnDummy = findViewById<Button>(R.id.btnrepo)
+
+        btnAboutProject.setOnClickListener {
+        }
+
+        btnDummy.setOnClickListener {
+            val linkTujuan = "https://github.com/Unalome7431/PrakPAB"
+
+            val intentBukaBrowser = Intent(Intent.ACTION_VIEW, Uri.parse(linkTujuan))
+            startActivity(intentBukaBrowser)
+        }
+    }
+}
